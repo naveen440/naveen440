71be6c72c35fe4acc2a1d5c6d9a62c4d8f216375
naveen440 <naveen_rajavardhan@srmuniv.edu.in>
2017-07-15 14:38:06 +0530

    Create oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,12 @@
+def main(aa,bb):
+	x=aa
+	y=bb
+	if(x%2==0):
+		print'The given number is even\n'
+	else:
+		print'The given number is odd'
+	if(y%2==1):
+		print'The number is odd\n'
+	else:
+		print'The number is even'
+main(36,66)
